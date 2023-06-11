96e3b381652fa4eb63750c8363a2e702479d4e5d
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-06-11 18:29:00 -0300

    setup

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+        }
+
+        //GET
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+        //GET pero solo 1
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+        }
+
+        //PUT
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
+
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza is null)
+                return NotFound();
+
+            PizzaService.Update(pizza);
+
+            return NoContent();
+        }
+
+        //DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza is null)
+                return NotFound();
+
+            PizzaService.Delete(id);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo nombre debe estar.")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "El campo libre de gluten debe estar.")]
+        public required bool IsGlutenFree { get; set; }
+    }
+}
